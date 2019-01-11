51aeafdbcbc67fb8ba5def4eb3798697d0319412
Evgeniy Kozlov <dsuchka@gmail.com>
2019-01-11 17:54:12 +0300

    tools: tuo-watch.sh: support new output format

--- tools/to-be-generalized/tuo-watch.sh
+++ tools/to-be-generalized/tuo-watch.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
 mask=${1:-tuo*log}
+watch="watch"
+if [[ $2 == cat ]]; then
+    watch="bash -c"
+fi
 
-exec watch "tail -n2 $mask | sed -r -e 's:\(([0-9.]+-[0-9.]+, )?([0-9]+ )+\/ ([0-9]+)\):(\1... / \3):'"
+exec $watch 'for i in '"$mask"'; do
+    echo " *** $(basename "$i") ***"
+    tail -n5 "$i" | \
+        grep -P "Deck improved|Optimized Deck|Upgraded|^\\d+ units" | \
+        tail -n2 | \
+        sed -r -e "s:\\(([0-9.]+-[0-9.]+, )?([0-9.]+ )+/ ([0-9.]+)\\):(\\1... / \\3):"
+    echo
+done'
